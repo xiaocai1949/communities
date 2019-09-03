6a6f2ba0d74f53774907f699e3433e7274b281e3
DELL <15821038483@163.com>
2019-09-03 13:59:52 +0800

    add index

--- src/main/java/cn/self/communities/contorller/IndexController.java
+++ src/main/java/cn/self/communities/contorller/IndexController.java
@@ -3,14 +3,12 @@ package cn.self.communities.contorller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloController {
-    @GetMapping("/hello")
-    public String hell(@RequestParam(name = "name")String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
+public class IndexController {
+    @GetMapping("/")
+    public String hell(){
+        return "index";
     }
 }
