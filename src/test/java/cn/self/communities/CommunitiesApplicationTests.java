a5717d1f0aba776d4f93cc7a2100ce24a1d7f1df
DELL <15821038483@163.com>
2019-09-03 13:38:46 +0800

    git init repo

---
+++ src/test/java/cn/self/communities/CommunitiesApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.self.communities;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommunitiesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
